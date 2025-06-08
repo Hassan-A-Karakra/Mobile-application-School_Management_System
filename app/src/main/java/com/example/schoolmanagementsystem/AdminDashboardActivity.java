36791de3b345bf441602628a8dcf1d91dac6b582
omardibas <1210243@student.birzeit.edu>
2025-06-08 12:54:49 +0300

    omar commit 8-6

--- app/src/main/java/com/example/schoolmanagementsystem/AdminDashboardActivity.java
+++ app/src/main/java/com/example/schoolmanagementsystem/AdminDashboardActivity.java
@@ -9,19 +9,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminDashboardActivity extends AppCompatActivity {
 
-    Button addStudentBtn, addTeacherBtn, viewStudentsBtn, viewTeachersBtn;
+    Button addTeacherBtn, viewStudentsBtn, viewTeachersBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
 
-        addStudentBtn = findViewById(R.id.addStudentBtn);
         addTeacherBtn = findViewById(R.id.addTeacherBtn);
         viewStudentsBtn = findViewById(R.id.viewStudentsBtn);
         viewTeachersBtn = findViewById(R.id.viewTeachersBtn);
 
-        addStudentBtn.setOnClickListener(v -> startActivity(new Intent(this, RegistrarActivity.class)));
         addTeacherBtn.setOnClickListener(v -> startActivity(new Intent(this, RegistrarActivity.class)));
         viewStudentsBtn.setOnClickListener(v -> startActivity(new Intent(this, StudentListActivity.class)));
         viewTeachersBtn.setOnClickListener(v -> startActivity(new Intent(this, TeacherListActivity.class)));
